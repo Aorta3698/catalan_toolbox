38258ec93c770d0000f00e4b90513592cec625b5
Zoe Winters <cost_placard748@knowingeyes.com>
2024-06-11 19:29:01 -0700

    prog: fallback to python3/python

--- install.sh
+++ install.sh
@@ -4,26 +4,27 @@ DIR=./visualizer
 PIP=$DIR/bin/pip
 PYTHON=python3
 
-if ! command -v $PYTHON $> /dev/null; then
+if ! command -v $PYTHON &> /dev/null; then
   PYTHON=python
 fi
 
-if ! command -v $PYTHON $> /dev/null; then
-  echo "python or python3 executable can't be found. Did you install Python?"
+if ! command -v $PYTHON &> /dev/null; then
+  echo >&2 "python or python3 executable can't be found. Did you install Python?"
+  exit 1
 fi
 
 if ! python -m venv $DIR; then
-  echo "Python virtual environment creation failed!"
+  echo >&2 "Python virtual environment creation failed!"
   exit 1
 fi
 
 if ! $PIP install -r $DIR/req.txt; then
-  echo "python package dependency installation failed!"
+  echo >&2 "python package dependency installation failed!"
   exit 1
 fi
 
 if ! make; then
-  echo "Makefile failed to compile!"
+  echo >&2 "Makefile failed to compile!"
   exit 1
 fi
 
