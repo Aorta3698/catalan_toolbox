c594fe787cf78b9378ac0004c09f134affebc2ce
Zoe Winters <cost_placard748@knowingeyes.com>
2024-06-11 20:17:33 -0700

    added: silence netgraph warnings during installation

--- install.sh
+++ install.sh
@@ -9,9 +9,14 @@ if ! command -v $PYTHON &> /dev/null; then
 fi
 
 if ! command -v $PYTHON &> /dev/null; then
-  echo >&2 "python or python3 executable can't be found. Did you install Python?"
+  echo >&2 "python or python3 executable can't be found. Is python in the PATH?"
   exit 1
 fi
+
+VIR=$($PYTHON --version)
+VIR=${VIR#Python *}
+VIR=${VIR%.*}
+NETGRAPH=$DIR/lib/python$VIR/site-packages/netgraph
 
 if ! $PYTHON -m venv $DIR; then
   echo >&2 "Python virtual environment creation failed!"
@@ -23,9 +28,17 @@ if ! $PIP install -r $DIR/req.txt; then
   exit 1
 fi
 
+if ! sed -i $'360i\\\tnp.seterr(divide="ignore", invalid="ignore")' $NETGRAPH/_utils.py; then
+  echo >&2 "Failed to silence warnings from Netgraph. It's no big deal though."
+fi
+
 if ! make; then
   echo >&2 "Makefile failed to compile!"
   exit 1
 fi
 
 printf "\nInstallation Success!"
+
+# https://stackoverflow.com/questions/5190966/using-sed-to-insert-tabs
+# https://stackoverflow.com/a/8394273
+# https://stackoverflow.com/a/19482947
